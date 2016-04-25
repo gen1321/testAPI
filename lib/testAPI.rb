dfa2de6b69c14858908152edcff48355a5eee403
gen1321 <gen1321@mail.ru>
2016-04-25 16:46:35 +0300

    init

--- lib/testAPI.rb
+++ lib/testAPI.rb
@@ -12,27 +12,28 @@ module TestAPI
     end
 
     def self.getAllUsers
-      requestBuilder('get', '/users').execute
+      JSON.parse(requestBuilder('get', '/users').execute)
     end
 
     def self.createNewUser(email)
-      requestBuilder('post','/users',email.to_json).execute
+      JSON.parse(requestBuilder('post','/users',email.to_json).execute)
     end
 
     def self.getUser(id)
-      requestBuilder('get','/users/'+id.to_s).execute
+      JSON.parse(requestBuilder('get','/users/'+id.to_s).execute)
     end
 
     def self.updateUser(id ,args)
-      requestBuilder('put','/users/'+id.to_s,args.to_json).execute
+      JSON.parse(requestBuilder('put','/users/'+id.to_s,args.to_json).execute)
     end
 
     def self.deleteUser(id)
-      requestBuilder('delete','/users/'+id.to_s).execute
+      JSON.parse(requestBuilder('delete','/users/'+id.to_s).execute)
     end
     private
     def self.requestBuilder(method = nil, url = '', body={} )
       request = RestClient::Request.new(method: method, user:  @@API_key, url: @@base_url+url, payload:body )
     end
+
   end
 end
--- spec/testAPI_spec.rb
+++ spec/testAPI_spec.rb
@@ -7,28 +7,33 @@ describe TestAPI do
     expect(TestAPI::VERSION).not_to be nil
   end
 
+
   it 'Basic http auth is working' do
     expect(user.testFire.code).to eq(200)
   end
 
+  it 'make sure response have a json format' do
+    expect(user.testFire.headers[:content_type]).to eq('application/json')
+  end
+
   it 'should receive all users' do
-   expect(user.getAllUsers).to include('users')
+   expect(user.getAllUsers["users"]).not_to eq({})
   end
 
   it'should retrive user with id' do
-    expect(user.getUser(1)).to include('email')
+    expect(user.getUser(2)["email"]).not_to eq({})
   end
 
   it 'should create new user' do
-    expect(user.createNewUser(email:'gen1321@mail.ru')).to include('new user recorded')
+    expect(user.createNewUser(email:'gen1321@mail.ru')["message"]).to include('new user recorded')
   end
 
   it 'should update user with id' do
-    expect(user.updateUser(1,email:'gen1321@mail.ru')).to include('user updated')
+    expect(user.updateUser(2,email:'gen1321@mail.ru')["message"]).to include('user updated')
   end
 
   it 'should delete user with id' do
-    expect(user.deleteUser(1)).to include('user deleted')
+    expect(user.deleteUser(4)["message"]).to include('user deleted')
   end
 
 end
